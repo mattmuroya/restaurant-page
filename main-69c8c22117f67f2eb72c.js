7f89529d3c71ce4e7a9fb2bd2796be3081035d08
mattmuroya <matt.muroya@gmail.com>
2022-02-08 02:29:40 -1000

    replace sass loader with postcss/tailwind

---
+++ main-69c8c22117f67f2eb72c.js
@@ -0,0 +1,1 @@
+(()=>{"use strict";const e=document.querySelector("body"),t=document.createElement("div");t.textContent="hello world",t.id="content",t.classList.add("container","bg-green-400","mx-auto","bg-green-400","my-10","p-10","rounded-md"),e.appendChild(t)})();
