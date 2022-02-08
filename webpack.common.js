3a1bd4796a242d6ccb015898a5c36e0c9b49fe01
mattmuroya <matt.muroya@gmail.com>
2022-02-07 22:48:18 -1000

    split dev and prod webpack config files

--- webpack.common.js
+++ webpack.common.js
@@ -2,9 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: 'production',
   entry: './src/index.js',
-  devtool: 'inline-source-map',
   plugins: [
     new HtmlWebpackPlugin({
       title: 'webpack Demo',
